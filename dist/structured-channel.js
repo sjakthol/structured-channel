e04c01140250e0d2d13d5e32ef22a60c373a54a3
Sami Jaktholm <sjakthol@outlook.com>
2015-08-15 15:12:24 +0300

    Update bundles.

--- dist/structured-channel.js
+++ dist/structured-channel.js
@@ -1,5 +1,14 @@
-var StructuredChannel =
-/******/ (function(modules) { // webpackBootstrap
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define(factory);
+	else if(typeof exports === 'object')
+		exports["StructuredChannel"] = factory();
+	else
+		root["StructuredChannel"] = factory();
+})(this, function() {
+return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
 
@@ -290,4 +299,6 @@ var StructuredChannel =
 	module.exports = StructuredChannel;
 
 /***/ }
-/******/ ]);
+/******/ ])
+});
+;
