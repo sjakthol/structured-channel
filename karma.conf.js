7a6f021c898d61863a3ced18b652bac436ac4cda
Sami Jaktholm <sjakthol@outlook.com>
2015-08-15 16:48:31 +0300

    Update karma configuration.

--- karma.conf.js
+++ karma.conf.js
@@ -1,11 +1,13 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    browsers: ['Chrome', 'Firefox'],
+    browsers: ['Chrome'],
     frameworks: ['mocha', 'chai'],
     files: [
       'dist/structured-channel.js',
-      'tests/helper_*',
+      { pattern: 'dist/structured-channel.js.map', included: false },
+      { pattern: 'tests/helper_*', included: false },
+      'tests/helper_functions.js',
       'tests/test_*.js'
     ],
   })
