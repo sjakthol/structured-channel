6c50b5d5956b04333b1d8bfa13fe16994acaf2f3
Sami Jaktholm <sjakthol@outlook.com>
2015-08-16 14:09:57 +0300

    Rename test_basic.js to test_exports.js.

--- tests/test_exports.js
+++ tests/test_exports.js
@@ -5,14 +5,4 @@ describe("StructuredChannel", function() {
     expect(StructuredChannel.connectTo).to.be.function;
     expect(StructuredChannel.waitForConnection).to.be.function;
   });
-
-  describe("connectTo()", function() {
-    it("should connect to a same-origin frame", function() {
-      return initializeChannelToFrame();
-    });
-
-    it("should connect to a worker", function() {
-      return initializeChannelToWorker();
-    });
-  });
 });
